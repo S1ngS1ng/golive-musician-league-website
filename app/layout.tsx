e90c0ee42394a630c1ff0def7260cde00713e96b
S1ngS1ng <liuxing0514@gmail.com>
2025-03-28 00:11:05 -0700

    add title and theme provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,11 +7,6 @@ import {
 } from "@mantine/core";
 import { theme } from "../theme";
 
-export const metadata = {
-  title: "Mantine Next.js template",
-  description: "I am using Mantine with Next.js!",
-};
-
 export default function RootLayout({ children }: { children: any }) {
   return (
     <html lang="en" {...mantineHtmlProps}>
@@ -22,9 +17,10 @@ export default function RootLayout({ children }: { children: any }) {
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
         />
+        <title>GoLive Musician&#39;s League</title>
       </head>
       <body>
-        <MantineProvider theme={theme}>{children}</MantineProvider>
+        <MantineProvider theme={theme} defaultColorScheme="auto">{children}</MantineProvider>
       </body>
     </html>
   );
