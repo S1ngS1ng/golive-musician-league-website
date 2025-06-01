55929850621c5498be1d384485a35dbf2febc4e5
S1ngS1ng <liuxing0514@gmail.com>
2025-05-31 19:54:25 -0700

    style fixes

--- components/nav-header/nav-header.tsx
+++ components/nav-header/nav-header.tsx
@@ -1,11 +1,13 @@
 "use client"
 
+import { type ReactElement } from "react";
 import Image from 'next/image';
 import { Burger, Group, Title } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import logo from '../../public/instruments.svg';
 import classes from './nav-header.module.css';
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const links = [
   { link: '/', label: 'Home' },
@@ -13,13 +15,15 @@ const links = [
   { link: '/events', label: 'Events' },
 ];
 
-export function NavHeader() {
+export function NavHeader(props: { children: ReactElement }) {
   const [opened, { toggle }] = useDisclosure(false);
+  const pathname = usePathname()
 
   const items = links.map((link) => (
     <Link
       key={link.label}
       href={link.link}
+      data-active={pathname === link.link || undefined}
       className={classes.link}>
       {link.label}
     </Link>
@@ -28,16 +32,20 @@ export function NavHeader() {
   return (
     <header className={classes.header}>
       <div className={classes.inner}>
-        <Group>
+        <Group wrap="nowrap">
           <Burger opened={opened} onClick={toggle} size="sm" hiddenFrom="sm" />
           <Image src={logo} alt={"logo"} className={classes.logo} />
           <Title order={2}>GoLive Musician&#39;s League</Title>
         </Group>
 
-        <Group>
+        <Group className={classes.navigation}>
           <Group ml={50} gap={5} className={classes.links} visibleFrom="sm">
             {items}
           </Group>
+        </Group>
+
+        <Group>
+          {props.children}
         </Group>
       </div>
     </header>
