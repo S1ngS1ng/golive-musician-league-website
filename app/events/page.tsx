b8b17e65e02aa60dee583f117910effb41ec54ca
Xing Liu <liuxing0514@gmail.com>
2025-11-17 22:37:51 -0800

    add about and event page (#2)

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,5 +1,31 @@
+import { Box, Container } from '@mantine/core';
+
 export default async function Events() {
   return (
-    <h2>Working in progress</h2>
+    <Container size={'lg'}>
+      <Box>
+        <h2>Weekend Jam Year-End Party | Proudly Presents GoLive</h2>
+        <h3>About GoLive Musician’s League</h3>
+        <p><b>GoLive Musician’s League</b> originated from a spontaneous music community called <b>“Weekend Jam.”</b> Founded in late 2023 by a group of friends who love singing, playing instruments, and sharing the joy of performing, GoLive is a registered <b>501(c)(3) nonprofit organization</b> in the United States.</p>
+        <p>Its mission is to <b>promote live music culture, support original music, empower emerging bands,</b> and <b>create more opportunities for music lovers to perform on stage.</b></p>
+        <p>Through events like <b>Weekend Jam</b>, we provide open stages for musicians, encourage artistic exchange, and bring together people in the community who share a passion for music—to connect, share, and create.</p>
+
+        <h3>Weekend Jam Year-End Party</h3>
+        <p>As its influence has grown, <b>Weekend Jam</b> has become a vital platform for grassroots music culture in Silicon Valley. Collaborating with multiple restaurants and bars across the Bay Area, we host live sessions every Saturday night that attract numerous musicians, singers, and music enthusiasts who come together to celebrate the freedom and passion of music.</p>
+        <p>To thank all the performers and fans who have participated in <b>Weekend Jam</b>, we warmly invite you to join our Year-End Music Party!</p>
+        <br/>
+        <p>📅 Date: Saturday, November 22, 2025, at 6:00 PM</p>
+        <p>📍 Venue: HalalStreet Hotpot Restaurant</p>
+        <p>5605 Mowry School Rd, Newark, CA 94560</p>
+        <p>🎶 Live Performances | 🍷 Food & Drinks | 🎤 Open Mic</p>
+        <p>👉 Tickets: $50 per person | Children under 12: $20</p>
+        <br/>
+        <p>GoLive’s events are made possible entirely through <b>volunteer efforts and donations</b>. All donations are <b>tax-deductible</b>, and you can donate through your company’s website on <a href={'https://benevity.com'}>benevity.com</a> by searching for <b>“GoLive
+          Musician’s League.”</b></p>
+        <br/>
+        <p>🎁 Donors who contribute <b>$400 or more this year (including company match)</b> will receive <b>free
+          admission</b> and a <b>special acknowledgment</b> at the event.</p>
+      </Box>
+    </Container>
   );
 }
