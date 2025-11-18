74843df86b8e3fda7394f9f6a7e9302c394d05b9
S1ngS1ng <liuxing0514@gmail.com>
2025-11-17 22:12:57 -0800

    add about and event page

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,9 @@ export default function RootLayout({
           <NavHeader>
             <NavThemeToggle />
           </NavHeader>
-          {children}
+          <main>
+            {children}
+          </main>
           <Footer/>
         </MantineProvider>
       </body>
